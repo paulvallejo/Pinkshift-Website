1c305abcc61026367be4c2d9fa3a6a231def97ab
Paul Vallejo <pvallej1@jhu.edu>
2020-11-26 21:15:56 -0500

    Updated the urls and views files to begin routing users to product detail pages. Converted the about page to be a product detail template page

--- Final_Project/store/urls.py
+++ Final_Project/store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('about/', views.aboutPage, name='about'),
+    path('', views.home, name='home'),
+    path('product/', views.productPage, name='product'),
 ]
--- Final_Project/store/views.py
+++ Final_Project/store/views.py
@@ -3,9 +3,29 @@ from django.http import HttpResponse
 
 
 def home(request):
+    """
+    This function combines a given template with a response object of certain rendered text.
+
+    **Parameters**
+        request:
+            A template
+
+    **Return**
+        Combination of the request object and template
+    """
     return render(request, 'home.html')
 
 
-def aboutPage(request):
-    return render(request, 'about.html')
+def productPage(request):
+    """
+    This function combines a given template with a response object of certain rendered text.
+
+    **Parameters**
+        request:
+            A template
+
+    **Return**
+        Combination of the request object and template
+    """
+    return render(request, 'product.html')
 # Create your views here.
