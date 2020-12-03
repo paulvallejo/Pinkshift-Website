be3c52096ab7856c825883719bdde2d7be4a2aca
Paul Vallejo <pvallej1@jhu.edu>
2020-12-03 02:58:04 -0500

    Wrote functions that are able to decrease quantity in cart, or remove product entirely

--- Final_Project/store/views.py
+++ Final_Project/store/views.py
@@ -69,7 +69,8 @@ def add_cart(request, product_id):
         cart.save()
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
+        if cart_item.quantity < cart_item.product.stock:
+            cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
@@ -91,3 +92,29 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
     except ObjectDoesNotExist:
         pass
     return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter))
+
+
+def cart_remove(request, product_id):
+    """
+    This function allows the user to decrease the quantity of a product at check out.
+    """
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity >1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart_detail')
+
+
+def cart_remove_product(request, product_id):
+    """
+    This function allows the user to decrease the quantity of a product at check out.
+    """
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart_detail')
